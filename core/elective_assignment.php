5464e41466a3eed81f862773c46f2870185d320d
Nguyen Quang Minh <quangminh.nguyen@mail.utoronto.ca>
2017-07-19 16:21:40 -0400

    admin interface update

--- core/elective_assignment.php
+++ core/elective_assignment.php
@@ -1,5 +1,6 @@
 <?php 
 $electives = get_list_of_electives();
+print_r($electives);
 
 /* Construct a list of table headers which are name of the electives. */
 $electives_str = '';
@@ -17,29 +18,51 @@ foreach($electives as $tuple) {
 
 	$table_body = '';
 	$student_list = get_list_of_students($tuple['electiveid'], 1);
+	// print_r($student_list);
 	foreach($student_list as $student) {
 		$table_body .= '<tr>
 							<td>'.$student['name'].'</td>
 							<td>'.$student['name'].'</td>
 							<td>'.$student['nick_name'].'</td>
 							<td>'.$student['email'].'</td>
-							<td>'.$student['class'].'</td>
-						</tr>';
+							<td>'.$student['class'].'</td>';
+
+		foreach($electives as $tuple) {
+			$electiveid = $tuple['electiveid'];
+			if (($electiveid == $student['elective1']) ||  ($electiveid == $student['elective2']) || ($electiveid == $student['elective3'])) {
+
+				if ($electiveid == $student['elective1']) {
+					$table_body .= '<td> <input type="radio" name="'.$student['name'].'" value= '.$electiveid.' checked> </td>';
+				} else {
+					$table_body .= '<td> <input type="radio" name="'.$student['name'].'" value= '.$electiveid.'> </td>';
+				}
+
+
+			} else {
+				$table_body .= '<td> <input type="radio" name="'.$student['name'].'" value= '.$electiveid.' disabled> </td>';
+			}
+		}
+
+		$table_body .= '</tr>';
+
 	}
 
 	echo '
-	<table class="table table-hover table-responsive">
-		<thead>
-			<tr> 
-			<th> First Name </th>
-			<th> Last Name </th>
-			<th> Nick Name </th>
-			<th> Email </th>
-			<th> Class </th>
-			'.$electives_str.'</tr>
-		</thead>
-		<tbody>'. $table_body. 
-		'</tbody>
-	</table>';
+	<form>
+		<table class="table table-hover table-responsive">
+			<thead>
+				<tr> 
+				<th> First Name </th>
+				<th> Last Name </th>
+				<th> Nick Name </th>
+				<th> Email </th>
+				<th> Class </th>
+				'.$electives_str.'</tr>
+			</thead>
+			<tbody>'. $table_body. 
+			'</tbody>
+		</table>
+		<button type="submit"> Confirm </button>
+	</form>';
 }
 ?>
--- head.php
+++ head.php
@@ -3,8 +3,9 @@
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
