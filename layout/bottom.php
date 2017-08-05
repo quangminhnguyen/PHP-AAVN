09b3ec7d115416b8d3665cf6e3bf49d8def51766
Nguyen Quang Minh <quangminh.nguyen@mail.utoronto.ca>
2017-08-05 16:49:33 -0400

    elective interface

--- layout/bottom.php
+++ layout/bottom.php
@@ -1,19 +1,31 @@
 <footer>
-	<div class="container-fluid">
+	<div class="container-fluid ft">
 		<div class="row"> 
-			<div class="col-md-9"> 
-				
+			<div class="col-md-9">
 			</div>
-			<div class="col-md-3"> 
-				<div class="circle-outline">
-					<a href="https://www.facebook.com/"> 
-						<span class="fa fa-3x fa-fw fa-facebook"></span>
-					</a>
-				</div>
-				<div class="circle-outline">
-						<a href="https://www.youtube.com/channel/UCUK2d56qFHFrhiykT6HA0Pw"> 
-							<span class="fa fa-3x fa-fw fa-youtube-play"> </span> 
+			<div class="col-md-3 logo-section">
+				<div class="logo-container">
+					<!-- School website. -->
+					<div class="circle-outline">
+						<a href="http://www.aavn.edu.vn/">
+							<i class="fa fa-3x fa-fw fa-institution" aria-hidden="true"> </i> 
 						</a>
+					</div>
+
+					<!-- School's facebook. -->
+					<div class="circle-outline">
+						<a href="https://www.facebook.com/ishcmc.aa"> 
+							<i class="fa fa-3x fa-fw fa-facebook" aria-hidden="true"></i>
+						</a>
+					</div>
+
+					<!-- Youtube aavn school's news.  -->
+					<div class="circle-outline">
+						<a  href="https://www.youtube.com/channel/UCUK2d56qFHFrhiykT6HA0Pw"> 
+							<i class="fa fa-3x fa-fw fa-youtube" aria-hidden="true"> </i> 
+						</a>
+					</div>
+
 				</div>
 			</div>
 		</div>
